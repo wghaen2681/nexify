7519c124c1e6958f97d4c0cb273991a52d2b9ca7
Neil Wang <wghaen2681@gmail.com>
2022-04-21 21:53:01 +0800

    [edit] before finish testing

--- src/components/BasicInfo.vue
+++ src/components/BasicInfo.vue
@@ -13,15 +13,19 @@
         </b-col>
       </b-row>
     </b-container>
-    <b-table class="mt-4 pb-4 text-left" responsive :fields="table_fields" :items="data_basicInfo">
+    <b-table class="[ b-table ] mt-4 pb-4 text-left" responsive style="overflow-y: visible !important;" :fields="table_fields" :items="data_basicInfo">
       <template #cell(name)="data"> <!-- #head 代表想要編輯 head 的內容 (name) 代表想要設定的是 name 資料欄位的內容 -->
         <input class="pl-2" style="height: 37px;" v-model="data.item.Name">
       </template>
       <template #cell(DateOfBirth)="data">
-        <b-form-datepicker class="h-100 border-secondary" style="min-width: max-content;" v-model="data.item.DateOfBirth"></b-form-datepicker>
+        <b-form-datepicker class="h-100 border-secondary" dropup style="min-width: max-content;" v-model="data.item.DateOfBirth"></b-form-datepicker>
       </template>
       <template #cell(Salary)="data">
-        <progress :value="data.item.Salary" :max="table_salary.max"></progress>
+        <div class="progress_container">
+          <!-- {{ data.item.Salary / table_salary.max * 100 }} -->
+          <b-progress :value="data.item.Salary" :max="table_salary.max"></b-progress>
+          <div class="progress_controller" :key="data.index" :style="'left: ' + (data.item.Salary / table_salary.max * 100) + '%'"></div>
+        </div>
       </template>
       <template #cell(address)="data">
         <input class="pl-2" style="height: 37px;" v-model="data.item.Address">
@@ -82,38 +86,122 @@ export default {
         Address: ''
       })
       this.data_add.display = true
+      this.drag_salary()
     },
     save_data: function () {
-      try {
-        this.$axios
-          .post('http://nexifytw.mynetgear.com:45000/api/Record/SaveRecords', this.data_basicInfo)
-          .then(() => {
-            this.get_data()
-          })
-          .catch(error => {
-            console.log(error)
-          })
-      } catch (error) {
-        console.log('儲存資料發生錯誤', error)
-      }
+      this.axios
+        .post('http://nexifytw.mynetgear.com:45000/api/Record/SaveRecords', this.data_basicInfo)
+        .then(() => {
+          this.get_data()
+          this.drag_salary()
+        })
+        .catch(error => {
+          console.log('儲存資料發生錯誤', error)
+        })
     },
-    get_data: async function () {
-      try {
-        const { data } = await this.$axios.get('http://nexifytw.mynetgear.com:45000/api/Record/GetRecords')
+    get_data: function () {
+      this.axios
+        .get('http://nexifytw.mynetgear.com:45000/api/Record/GetRecords')
+        .then(function (data) {
+          this.data_basicInfo = data.data.Data
+          this.data_add.display = false
+          this.table_salary.max = Math.max(...this.data_basicInfo.map(item => item.Salary))
+          this.drag_salary()
+        }.bind(this))
+        .catch(error => {
+          console.log('取得資料發生錯誤', error)
+        })
+    },
+    drag_salary: function () {
+      const $this = this
 
-        this.data_basicInfo = data.Data
-        this.data_add.display = false
+      setTimeout(function () {
+        $this.$('.progress_controller').draggable({
+          axis: 'x',
+          drag: function () {
+            const index = Array.prototype.indexOf.call(this.closest('tr').parentNode.childNodes, this.closest('tr'))
 
-        // console.log(this.data_basicInfo)
-        this.table_salary.max = Math.max(...this.data_basicInfo.map(item => item.Salary))
-        console.log(this.table_salary.max)
-      } catch (error) {
-        console.log('取得資料發生錯誤', error)
-      }
+            switch (true) {
+              case (this.offsetLeft >= 160):
+                $this.change_salary(index, 160)
+                break
+              case (this.offsetLeft >= 0):
+                $this.change_salary(index, this.offsetLeft)
+                break
+              case (this.offsetLeft < 0):
+                $this.change_salary(index, 0)
+                break
+            }
+          },
+          stop: function () {
+            console.log(this.offsetLeft)
+            switch (true) {
+              case (this.offsetLeft >= 160):
+                this.style.left = '160px'
+                break
+              case (this.offsetLeft < 0):
+                this.style.left = 0
+                break
+            }
+          }
+        })
+
+        // console.log(progressController[0].draggable())
+
+        // progressController.forEach(el => {
+        //   el.draggable({
+        //     axis: 'x', // 限制拖曳僅在 x 軸上
+        //     // create: function () {},
+        //     // start: function () {},
+        //     // drag: function () {},
+        //     stop: function () {
+        //       const posBasicInfo = el.position().left
+
+        //       switch (true) {
+        //         case (posBasicInfo >= 90):
+        //           Element.css({ left: 90 })
+        //           break
+        //         case (posBasicInfo < (-40)):
+        //           el.css({ left: -40 })
+        //           break
+        //       }
+        //     }
+        //   })
+        // })
+      }, 500)
+
+      // progressController.draggable({
+      //   axis: 'x', // 限制拖曳僅在 x 軸上
+      //   // create: function () {},
+      //   // start: function () {},
+      //   // drag: function () {},
+      //   stop: function () {
+      //     const posBasicInfo = progressController.position().left
+
+      //     switch (true) {
+      //       case (posBasicInfo >= 90):
+      //         progressController.css({ left: 90 })
+      //         break
+      //       case (posBasicInfo < (-40)):
+      //         progressController.css({ left: -40 })
+      //         break
+      //     }
+      //   }
+      // })
+    },
+    change_salary: function (index, data) {
+      // console.log(index, data)
+      const alteration = Math.round(data / 160 * this.table_salary.max)
+      // console.log(alteration)
+      this.data_basicInfo[index - 1].Salary = alteration
+      // console.log(this.data_basicInfo[index - 1].Salary )
     }
   },
-  async mounted () {
+  created () {
     this.get_data()
+  },
+  mounted () {
+    this.drag_salary()
   }
 }
 </script>
@@ -122,15 +210,42 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+j
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
+}
+
+.b-table {
+  min-height: 450px;
+}
+
+.progress_container {
+  position: relative;
+}
+
+.progress {
+  width: 160px;
+  height: 8px;
+  border: 1px solid #ccc;
+}
+
+.progress_controller {
+  position: absolute;
+  top: calc(50% - 15px / 2);
+  transform: translateX(-50%);
+  width: 15px;
+  aspect-ratio: 1/1;
+  background: #007bff;
+  border-radius: 50%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,14 +5,20 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios' // 引用 axios 套件
+import jquery from 'jquery'
+import jqui from 'jquery-ui-dist/jquery-ui'
+import jquiStyle from 'jquery-ui-dist/jquery-ui.min.css'
 
 import '@/style/style.css' // 引用自定義的 css 樣式
 
 Vue.config.productionTip = false
 
-Vue.prototype.$axios = axios
+Vue.prototype.axios = axios
+Vue.prototype.$ = jquery
 
 new Vue({
   router,
+  jqui,
+  jquiStyle,
   render: h => h(App)
 }).$mount('#app')
